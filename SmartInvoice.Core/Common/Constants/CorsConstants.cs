62c113078b95ff73b61277707a9a1781697bb887
Fedi Riahi <Aziz@gmail.com>
2026-01-07 19:10:16 +0100

    Invoice Service on V1.1

---
+++ SmartInvoice.Core/Common/Constants/CorsConstants.cs
@@ -0,0 +1,16 @@
+﻿// Constants/CorsConstants.cs
+namespace SmartInvoice.Invoice.Constants
+{
+    public static class CorsConstants
+    {
+        public const string AllowFrontendPolicy = "AllowFrontend";
+
+        // Development URLs
+        public static readonly string[] AllowedOrigins =
+        {
+            "http://localhost:3000",    // React dev server
+            "http://localhost:5173",    // Vite dev server
+            "http://localhost:4200"     // Angular dev server
+        };
+    }
+}
